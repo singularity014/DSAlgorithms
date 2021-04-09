92be889c06368f21b1551d155fcd6b644e96ef38
psharma92 <prafull.sharma@naister.com>
2021-04-09 22:00:52 +0530

    adding Binary Search Algorithm...

---
+++ Search_Algorithms/binary_search.py
@@ -0,0 +1,37 @@
+"""
+author: Praful
+code: Binary Sear
+"""
+
+def binary_search(item, sorted_list):
+    """
+    given an item to find and a sorted list,
+    search the item in the list and
+    reutrn the position of the item in the list
+    :param item: item to search
+    :param sorted_list: sorted list (Binary search always requires sorted list)
+    :return: position/None based on found or not
+    """
+    low = 0
+    high = len(sorted_list) - 1
+
+    while low <= high:
+        mid = (low + high)//2
+        guess_number = sorted_list[mid]
+
+        if guess_number == item:
+            return mid
+        if guess_number > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+
+if __name__ == "__main__":
+
+    search_item = 7
+    search_in_list = [1, 3, 5, 7, 9, 10]
+    res = binary_search(search_item, search_in_list)
+    print(res)
